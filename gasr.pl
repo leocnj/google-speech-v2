70ca0e1f9631b523e4b8caecac21d8cc5797dec4
Chen, Lei <LChen@ETS.ORG>
2015-05-01 23:39:21 -0400

    start
    Will follow gasr.pl to update the Perl code to use the new Bing ASR API

---
+++ gasr.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#
+# based on http://bit.ly/10b6jx9
+#
+# TODO: convert to a Python version suing urllib2
+#
+# 4/23/2013  This un-offical API still works.
+#
+require LWP::UserAgent;
+
+my $url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US";
+my $audio = "";
+
+open(FILE, "<" . $ARGV[0]);
+while(<FILE>)
+{
+    $audio .= $_;
+}
+close(FILE);
+
+my $ua = LWP::UserAgent->new;
+
+my $response = $ua->post($url, Content_Type => "audio/x-flac; rate=16000", Content => $audio);
+if ($response->is_success)
+{
+    print $response->content;
+}
+
+1;
